0c6e7809fc341a8f2ba0fe2a9ec382c183ad833e
reeenanm <renan.amonteiro@gmail.com>
2024-10-10 15:52:53 -0300

    Update obter_access_token.py

--- obter_access_token.py
+++ obter_access_token.py
@@ -4,7 +4,7 @@ import requests
 CLIENT_ID = '7511527097985348'
 CLIENT_SECRET = 'IvUCyebIc9QqDrLKxwPOANMFE82p8Gz8'
 AUTHORIZATION_CODE = 'TG-67082132af8165000135175b-1281315022'
-REDIRECT_URI = 'https://projetoadam-production.up.railway.app/'
+REDIRECT_URI = 'https://projetoadam-production.up.railway.app'
 
 url = 'https://api.mercadolibre.com/oauth/token'
 
